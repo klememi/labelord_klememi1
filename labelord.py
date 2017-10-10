923a0427e47277c4b626f110c99f60f5bbc0f2e3
Michal Klement <klememi1@fit.cvut.cz>
2017-10-11 01:25:47 +0200

    first steps looking good. list_repos almost done

--- labelord.py
+++ labelord.py
@@ -4,20 +4,34 @@
 # TODO: create requirements.txt and install
 import click
 import requests
-
+import configparser
 # Structure your implementation as you want (OOP, FP, ...)
 # Try to make it DRY also for your own good
 
 
 @click.group('labelord')
+@click.option('-c', '--config', default='./config.cfg', help='Configuration file path.')
+@click.option('-t', '--token', envvar='GITHUB_TOKEN', default='', help='GitHub token.')
 @click.pass_context
-def cli(ctx):
+def cli(ctx, config, token):
     # TODO: Add and process required app-wide options
     # You can/should use context 'ctx' for passing
     # data and objects to commands
-
+    cfgparser = configparser.ConfigParser()
+    cfgparser.read(config)
+    github_token = token if token else cfgparser['github']['token']
     # Use this session for communication with GitHub
-    session = ctx.obj.get('session', requests.Session())
+    ctx.obj['token'] = github_token
+    ctx.obj['session'] = requests.Session()
+    session = ctx.obj.get('session')
+    session.headers = {'User-Agent': 'mi-pyt-01-labelord'}
+    session.auth = token_auth
+
+
+@click.pass_context
+def token_auth(ctx, req):
+    req.headers['Authorization'] = 'token ' + ctx.obj.get('token')
+    return req
 
 
 @cli.command()
@@ -25,7 +39,11 @@ def cli(ctx):
 def list_repos(ctx):
     # TODO: Add required options/arguments
     # TODO: Implement the 'list_repos' command
-    ...
+    session = ctx.obj.get('session')
+    r = session.get('https://api.github.com/user/repos?per_page=100&page=1')
+    print(r.status_code)
+    for i in r.json():
+        print(i['full_name'])
 
 
 @cli.command()
