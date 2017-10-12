50348cdd88a678f6d39ec6427864db2f511e3bdb
Michal Klement <klememi1@fit.cvut.cz>
2017-10-12 21:26:05 +0200

    pre-final version, needs (a lot) refactoring though

--- labelord.py
+++ labelord.py
@@ -19,39 +19,8 @@ def cli(ctx, config, token, colorful):
     cfg.read(config)
     cfgtoken = cfg.get('github', 'token', fallback='')
     ctx.obj['token'] = token if token else cfgtoken
-    # ctx.obj['session'] = requests.Session()
     ctx.obj['colorful'] = colorful
     ctx.obj['config'] = cfg
-
-
-@click.pass_context
-def set_session(ctx):
-    session = ctx.obj.get('session', requests.Session())
-    session.headers = {'User-Agent': 'mi-pyt-01-labelord'}
-    github_token = ctx.obj.get('token')
-    if not github_token:
-        error(3, 'No GitHub token has been provided')
-    def token_auth(req):
-        req.headers['Authorization'] = 'token ' + github_token
-        return req
-    session.auth = token_auth
-    ctx.obj['session'] = session
-
-
-def error(return_value, *args, **kwargs):
-    if args:
-        print(*args, file=sys.stderr, **kwargs)
-    if return_value > 0:
-        sys.exit(return_value)
-
-
-@click.pass_context
-def get_response(ctx, uri):
-    session = ctx.obj.get('session', requests.Session())
-    response = session.get('https://api.github.com/' + uri)
-    if response.status_code == 401:
-        error(4, 'GitHub: ERROR {} - {}'.format(response.status_code, response.json()['message']))
-    return response, response.status_code
 
 
 @cli.command()
@@ -109,44 +78,85 @@ def run(ctx, mode, all_repos, dry_run, verbose, quiet, template_repo):
     logging = 0
     logging += 1 if verbose else 0
     logging += 2 if quiet else 0
-    update(repos, labels, dry_run, logging) if mode == 'update' else replace(repos, labels, dry_run, logging)
+    perform_operation(True if mode == 'replace' else False, repos, labels, dry_run, logging)
+
+
+@click.pass_context
+def set_session(ctx):
+    session = ctx.obj.get('session', requests.Session())
+    session.headers = {'User-Agent': 'mi-pyt-01-labelord'}
+    github_token = ctx.obj.get('token')
+    if not github_token:
+        error(3, 'No GitHub token has been provided')
+    def token_auth(req):
+        req.headers['Authorization'] = 'token ' + github_token
+        return req
+    session.auth = token_auth
+    ctx.obj['session'] = session
+
+
+def error(return_value, *args, **kwargs):
+    if args:
+        print(*args, file=sys.stderr, **kwargs)
+    if return_value > 0:
+        sys.exit(return_value)
+
+
+@click.pass_context
+def get_response(ctx, uri):
+    session = ctx.obj.get('session', requests.Session())
+    response = session.get('https://api.github.com/' + uri)
+    if response.status_code == 401:
+        error(4, 'GitHub: ERROR {} - {}'.format(response.status_code, response.json()['message']))
+    return response, response.status_code
 
 
 @click.pass_context
 def update_label(ctx, repo, label, data):
     session = ctx.obj.get('session', requests.Session())
     response = session.patch('https://api.github.com/repos/{}/labels/{}'.format(repo, label), data)
-    return response.status_code
+    return response.status_code, response.json()['message']
 
 
 @click.pass_context
 def add_label(ctx, repo, data):
     session = ctx.obj.get('session', requests.Session())
     response = session.post('https://api.github.com/repos/{}/labels'.format(repo), data)
-    return response.status_code
+    return response.status_code, response.json()['message']
 
 
-def update(repos, labels, dry_run, logging):
+@click.pass_context
+def delete_label(ctx, repo, label):
+    session = ctx.obj.get('session', requests.Session())
+    response = session.delete('https://api.github.com/repos/{}/labels/{}'.format(repo, label))
+    return response.status_code, response.json()['message']
+
+
+def perform_operation(replace, repos, labels, dry_run, logging):
     errors = 0
     update_repos = set()
     for repo in repos:
         response, code = get_response('repos/{}/labels?per_page=100&page=1'.format(repo))
         if code == 200:
+            update_repos.add(repo)
             repo_labels = {label['name']: label['color'] for label in response.json()}
+            labels_to_delete = set()
+            if replace:
+                labels_to_delete = {label for label in repo_labels} - {label for label in labels}
             for label in labels:
                 if label in repo_labels.keys():
                     if labels[label] != repo_labels[label]:
                         if not dry_run:
                             update_data = {"name": label, "color": labels[label]}
-                            response_code = update_label(repo, label, update_data)
+                            response_code, message = update_label(repo, label, update_data)
                             if response_code == 200:
                                 print('[UPD][SUC] {}; {}; {}'.format(repo, label, labels[label]))
                                 update_repos.add(repo)
                             elif logging == 1:
-                                print('[UPD][ERR] {}; {}; {}'.format(repo, label, labels[label]))
+                                print('[UPD][ERR] {}; {}; {}; {} - {}'.format(repo, label, labels[label], response_code, message))
                                 errors += 1
                             elif logging == 0 or logging == 3:
-                                error(0, 'ERROR: UPD; {}; {}; {}; {} - {}'.format(repo, label, labels[label], code, response.json()['message']))
+                                error(0, 'ERROR: UPD; {}; {}; {}; {} - {}'.format(repo, label, labels[label], response_code, message))
                                 errors += 1
                         else:
                             update_repos.add(repo)
@@ -155,21 +165,36 @@ def update(repos, labels, dry_run, logging):
                 else:
                     if not dry_run:
                         add_data = {"name": label, "color": labels[label]}
-                        response_code = add_label(repo, add_data)
-                        if response_code == 200:
+                        response_code, message = add_label(repo, add_data)
+                        if response_code == 201:
                             print('[ADD][SUC] {}; {}; {}'.format(repo, label, labels[label]))
                             update_repos.add(repo)
                         elif logging == 1:
-                            print('[ADD][ERR] {}; {}; {}'.format(repo, label, labels[label]))
+                            print('[ADD][ERR] {}; {}; {}; {} - {}'.format(repo, label, labels[label], response_code, message))
                             errors += 1
                         elif logging == 0 or logging == 3:
-                            error(0, 'ERROR: ADD; {}; {}; {}; {} - {}'.format(repo, label, labels[label], code, response.json()['message']))
+                            error(0, 'ERROR: ADD; {}; {}; {}; {} - {}'.format(repo, label, labels[label], response_code, message))
                             errors += 1
                     else:
                         update_repos.add(repo)
                         if logging == 1:
                             print('[ADD][DRY] {}; {}; {}'.format(repo, label, labels[label]))
-
+            for label in labels_to_delete:
+                if not dry_run:
+                    response_code, message = delete_label(repo, label)
+                    if response_code == 204:
+                        print('[DEL][SUC] {}; {}; {}'.format(repo, label, labels[label]))
+                        update_repos.add(repo)
+                    elif logging == 1:
+                        print('[DEL][ERR] {}; {}; {}; {} - {}'.format(repo, label, labels[label], response_code, message))
+                        errors += 1
+                    elif logging == 0 or logging == 3:
+                        error(0, 'ERROR: DEL; {}; {}; {}; {} - {}'.format(repo, label, labels[label], response_code, message))
+                        errors += 1
+                else:
+                    update_repos.add(repo)
+                    if logging == 1:
+                        print('[DEL][DRY] {}; {}; {}'.format(repo, label, labels[label]))
         elif logging == 1:
             error(0, '[LBL][ERR] {}; {} - {}'.format(repo, code, response.json()['message']))
             errors += 1
@@ -187,11 +212,6 @@ def update(repos, labels, dry_run, logging):
         print('[SUMMARY] {} repo(s) updated successfully'.format(len(update_repos)))
     elif logging == 0 or logging == 3:
         print('SUMMARY: {} repo(s) updated successfully'.format(len(update_repos)))
-
-
-
-def replace(repos, labels, dry_run, logging):
-    print()
 
 
 def get_labels(template_repository, config):
