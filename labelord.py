3f4dfc6d3f42e04cad3f9401fc0abead2321056b
Michal Klement <klememi1@fit.cvut.cz>
2017-10-13 23:47:46 +0200

    initial commit for task 2

--- labelord.py
+++ labelord.py
@@ -5,6 +5,8 @@ import click
 import requests
 import configparser
 import sys
+import flask
+import os
 
 
 @click.group('labelord')
@@ -246,7 +248,53 @@ def get_repos(all_repos, config):
         return [repo for repo in config['repos'] if config['repos'].getboolean(repo)]
     else:
         error(7, 'No repositories specification has been found')
+######################################################################
+######################################################################
+class LabelordWeb(flask.Flask):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # You can do something here, but you don't have to...
+        # Adding more args before *args is also possible
+        # You need to pass import_name to super as first arg or
+        # via keyword (e.g. import_name=__name__)
+        # Be careful not to override something Flask-specific
+        # @see http://flask.pocoo.org/docs/0.12/api/
+        # @see https://github.com/pallets/flask
+
+    def inject_session(self, session):
+        # TODO: inject session for communication with GitHub
+        # The tests will call this method to pass the testing session.
+        # Always use session from this call (it will be called before
+        # any HTTP request). If this method is not called, create new
+        # session.
+        ...
+
+    def reload_config(self):
+        # TODO: check envvar LABELORD_CONFIG and reload the config
+        # Because there are problems with reimporting the app with
+        # different configuration, this method will be called in
+        # order to reload configuration file. Check if everything
+        # is correctly set-up
+        ...
 
 
+# TODO: instantiate LabelordWeb app
+# Be careful with configs, this is module-wide variable,
+# you want to be able to run CLI app as it was in task 1.
+app = ...
+
+# TODO: implement web app
+# hint: you can use flask.current_app (inside app context)
+
+
+@cli.command()
+@click.pass_context
+def run_server(ctx):
+    # TODO: implement the command for starting web app (use app.run)
+    # Don't forget to app the session from context to app
+    ...
+#######################################################################
+#######################################################################
 if __name__ == '__main__':
     cli(obj={})
