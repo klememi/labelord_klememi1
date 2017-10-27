eddf15c24526cccbf613843e70a8894d53864473
Michal Klement <klememi1@fit.cvut.cz>
2017-10-27 17:36:53 +0200

    necessary edits

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 with open('README') as f:
@@ -6,15 +6,15 @@ with open('README') as f:
 
 
 setup(
-    name='labelord',
+    name='labelord_klememi1',
     version='0.3',
-    description='Synchronizes label in configurre GitHub repositories.',
+    description='Synchronizes label in configured GitHub repositories.',
     long_description=long_description,
     author='Michal Klement',
     author_email='klememi1@fit.cvut.cz',
     license='MIT License',
     url='https://github.com/klememi/labelord_klememi1',
-    packages=find_packages(),
+    packages=['labelord'],
     package_data={'labelord': ['templates/*.html', 'static/bg.jpg']},
     python_requires='~=3.6',
     install_requires=['click>=6.7', 'Flask>=0.12.2', 'requests>=2.18.4'],
@@ -29,6 +29,7 @@ setup(
         'Intended Audience :: Education',
         'License :: OSI Approved :: MIT License',
         'Environment :: Console',
+        'Environment :: Web Environment',
         'Framework :: Flask',
         'Topic :: Utilities',
         'Topic :: Education',
